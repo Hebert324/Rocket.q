3e0c9b67d60e0315a081e84d5a406c82eb608a2b
Hebert324 <hebertrocha76@gmail.com>
2021-06-24 20:39:33 -0300

    Update init.js

--- src/db/init.js
+++ src/db/init.js
@@ -1,22 +1,22 @@
 const Database = require("./config")
 
 const initDb = {
-    async init() {
+    async init(){
         const db = await Database()
 
         await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
-            pass TEXT 
+            pass TEXT
         )`);
 
         await db.exec(`CREATE TABLE questions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo TEXT,
-            read INT
+            read INT 
         )`);
 
         await db.close()
     }
 }
 
-initDb.init()
+initDb.init();
