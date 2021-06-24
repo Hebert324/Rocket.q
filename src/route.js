1c10896622c06d4c371033123c2cdd3d8b21b642
Hebert324 <hebertrocha76@gmail.com>
2021-06-24 11:43:31 -0300

    integrated node

--- src/route.js
+++ src/route.js
@@ -6,6 +6,9 @@ const route = express.Router()
 
 //aqui falamos que a rota que ele vai pegar ele precisa adicionar uma barra e pegar o arquivo index que esta na pasta view.
 route.get('/', (req, res) => res.render('index'))
+//adicionando a rota dos outros arquivos
+route.get('/room', (req, res) => res.render('room'))
+route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 //aqui estamos exportando a route
 module.exports = route
