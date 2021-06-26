9287a93f86390bb2a8a031853c28162442a393b6
Hebert324 <hebertrocha76@gmail.com>
2021-06-26 15:06:00 -0300

    ultimo dia

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,22 +5,38 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(let i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        while(isRoom) {
+            // Aqui ele gera o id da sala
+            for(let i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            // Verificar se o id ja existi no banco de dados
+            const roomExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomExistIds.some(roomExistIds => roomExistIds === roomId)
+
+            if(!isRoom){
+                // Inseri a sala e a senha no banco de dados
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close()
 
         res.redirect(`/room/${roomId}`)
+    },
+
+    open(req, res) {
+        const roomId = req.params.room
+        res.render('room', {roomId: roomId})
     }
 }
--- src/route.js
+++ src/route.js
@@ -11,12 +11,13 @@ route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
 //adicionando a rota dos outros arquivos
 route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render('room'))
-
 //get e pegar o post e enviar
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+
 //formato que o formulario de dentro da modal tem que passar a informação
 route.post('/question/:room/:question/:action', QuestionController.index)
-route.post('/create-room', RoomController.create)
+route.post('/question/create/:room', QuestionController.create)
 
 //aqui estamos exportando a route
 module.exports = route
